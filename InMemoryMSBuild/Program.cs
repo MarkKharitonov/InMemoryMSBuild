72a3cf69d7318e36d4db0549898e0d76c6d6ab00
Kharitonov, Mark <mark.kharitonov@ceridian.com>
2021-10-07 22:43:28 -0400

    Initial commit

---
+++ InMemoryMSBuild/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InMemoryMSBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
