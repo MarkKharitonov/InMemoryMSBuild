c7d4642042b5de552e0b9d3017d63145b7425be5
Kharitonov, Mark <mark.kharitonov@ceridian.com>
2021-10-07 23:20:43 -0400

    Show that using MSBuild to parse byte array produces invalid project

--- InMemoryMSBuild/Program.cs
+++ InMemoryMSBuild/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Xml;
 
 namespace InMemoryMSBuild
 {
@@ -13,10 +14,20 @@ namespace InMemoryMSBuild
         {
             var sut = Path.GetFullPath(Path.Combine(Assembly.GetExecutingAssembly().Location, @"..\..\..\..\..\LegacyDotNetSUT\LegacyDotNetSUT.csproj"));
             Debug.Assert(File.Exists(sut));
+
+            Console.WriteLine("Reading from disk");
             var project = new Project(sut);
             foreach (var filePath in project.GetItems("Compile").Select(o => o.GetMetadataValue("FullPath")))
             {
-                Console.WriteLine(filePath);
+                Console.WriteLine($"File.Exists(\"{filePath}\") = {File.Exists(filePath)}");
+            }
+            ProjectCollection.GlobalProjectCollection.UnloadAllProjects();
+
+            Console.WriteLine("Reading from memory");
+            project = new Project(XmlReader.Create(new MemoryStream(File.ReadAllBytes(sut))));
+            foreach (var filePath in project.GetItems("Compile").Select(o => o.GetMetadataValue("FullPath")))
+            {
+                Console.WriteLine($"File.Exists(\"{filePath}\") = {File.Exists(filePath)}");
             }
         }
     }
